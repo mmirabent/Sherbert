6349c08e61bb995e06e9b95b8c6403107f22cd93
Marcos Mirabent <mmirabent@gmail.com>
2015-01-06 20:18:02 -0500

    Added rate limiting

--- app.rb
+++ app.rb
@@ -1,8 +1,11 @@
 require 'sinatra'
 require 'haml'
 require 'json'
+require 'rack/throttle'
 require_relative 'bot' # Comment this line to not run the bot
 require_relative 'model'
+
+use Rack::Throttle::Interval, :min => 1.0
 
 get '/' do
   @titles = Title.order_by(:votes).reverse
