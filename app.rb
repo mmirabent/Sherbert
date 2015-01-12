e332b58478c9bd313fee63bdb26044c2f270014d
Marcos Mirabent <mmirabent@gmail.com>
2015-01-12 02:04:54 -0500

    New post handler and model validations Added post handler for adding new titles via a very simple API. Also added name presence validation to the sequel model

--- app.rb
+++ app.rb
@@ -7,6 +7,11 @@ require_relative 'model'
 
 use Rack::Throttle::Interval, :min => 1.0
 
+before do
+  request.body.rewind
+  @request_payload = JSON.parse(request.body.read)
+end
+
 get '/' do
   @titles = Title.order_by(:votes).reverse
   haml :titles
@@ -17,4 +22,16 @@ post '/vote/:id' do
   title.upvote
   title.save
   {:id => params[:id], :votes => title.votes}.to_json
+end
+
+post '/vote' do
+  title = Title.new
+  title.name = @request_payload["name"]
+  title.title = @request_payload["title"]
+  if title.valid?
+    title.save
+    {:id => title.id, :name => title.name, :title => title.title}.to_json
+  else
+    {:errors => title.errors.full_messages}.to_json
+  end
 end
