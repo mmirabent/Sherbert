704f8a892a2a707203f1966ffa2daaa3d3d279e1
Marcos Mirabent <mmirabent@gmail.com>
2015-01-12 19:11:30 -0500

    Moved the JSON before handler I was getting an error with empty get requests from the JSON before handler. Since I was only using the JSON parser in one handler, I moved the JSON parsing code there

--- app.rb
+++ app.rb
@@ -1,16 +1,12 @@
 require 'sinatra'
 require 'haml'
 require 'json'
+require 'sinatra/json'
 require 'rack/throttle'
-require_relative 'bot' # Comment this line to not run the bot
+# require_relative 'bot' # Comment this line to not run the bot
 require_relative 'model'
 
 use Rack::Throttle::Interval, :min => 1.0
-
-before do
-  request.body.rewind
-  @request_payload = JSON.parse(request.body.read)
-end
 
 get '/' do
   @titles = Title.order_by(:votes).reverse
@@ -21,17 +17,19 @@ post '/vote/:id' do
   title = Title[params[:id]]
   title.upvote
   title.save
-  {:id => params[:id], :votes => title.votes}.to_json
+  json({:id => params[:id], :votes => title.votes})
 end
 
 post '/vote' do
+  request.body.rewind
+  request_payload = JSON.parse(request.body.read)
   title = Title.new
-  title.name = @request_payload["name"]
-  title.title = @request_payload["title"]
+  title.name = request_payload["name"]
+  title.title = request_payload["title"]
   if title.valid?
     title.save
-    {:id => title.id, :name => title.name, :title => title.title}.to_json
+    json({:id => title.id, :name => title.name, :title => title.title})
   else
-    {:errors => title.errors.full_messages}.to_json
+    halt 400, json({:errors => title.errors.full_messages})
   end
 end
