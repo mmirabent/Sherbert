d8de3f2326394f658b4950e8e9029060165492ea
Marcos Mirabent <mmirabent@gmail.com>
2015-01-15 16:53:28 -0500

    Made the form work Added helper methods for making alerts and changing validation styles. Also added code to show errors from the server properly. Also added a lot of comments

--- public/app.js
+++ public/app.js
@@ -1,5 +1,77 @@
 $(document).ready(function () {
   "use strict";
+  
+  // Form logic
+  $('.form-inline').submit(function (event) {
+    var titleInput, nameInput, suggestion, valid = true;
+    
+    // Remove any error styles and alerts
+    $(".form-group").removeClass("has-error");
+    $(".alert").remove();
+    
+    // Get data from the form elements
+    titleInput = $("#titleInput").val();
+    nameInput = $("#nameInput").val();
+    
+    // Helper function for adding alerts
+    function addErrorAlert(errorMessage) {
+      //TODO: add a hidden div to the titles.haml file and clone it with jQuery instead of having all this HTML in here
+      var alertDiv = '<div class="alert alert-danger alert-dismissable" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button><strong>Error!</strong> ' + errorMessage + '</div>';
+      $(".suggestion-form").prepend(alertDiv);
+    }
+    
+    // Helper function for adding error style
+    function addErrorStyle(jQuerySelector) {
+      $(jQuerySelector).parent().addClass("has-error");
+    }
+    
+    // Called when the AJAX call returns success
+    function suggestionSuccessCallback(response) {
+      // Some code to refresh the page
+      alert("woo");
+    }
+    
+    // Called when the AJAX call returns with an error
+    function suggestionErrorCallback(response) {
+      // Add alerts at the top og the page for errors returned from the server
+      // console.log(response.responseJSON.errors);
+      response.responseJSON.errors.forEach(function (message,index,arr) {
+        addErrorAlert(message);
+      });
+    }
+    
+    // If the title is empty
+    if (!titleInput || titleInput === "") {
+      addErrorStyle("#titleInput");
+      addErrorAlert("Title can't be blank");
+      valid = false;
+    }
+    
+    // If name is empty
+    if (!nameInput || nameInput === "") {
+      addErrorStyle("#nameInput");
+      addErrorAlert("name can't be blank");
+      valid = false;
+    }
+    
+    // If we encountered any validation errors, stop right here
+    if(!valid) {
+      return;
+    }
+    
+    // Build the suggestion object
+    suggestion = {"name":nameInput, "title":titleInput};
+    
+    $.ajax({
+      url: 'vote',
+      type: 'POST',
+      dataType: 'json',
+      data: JSON.stringify(suggestion),
+      success: suggestionSuccessCallback,
+      error: suggestionErrorCallback
+    }); // AJAX call
+  }); // Form submit callback
+  
   $('button').click(function (event) {
     // set up
     var titleId, element, votes;
